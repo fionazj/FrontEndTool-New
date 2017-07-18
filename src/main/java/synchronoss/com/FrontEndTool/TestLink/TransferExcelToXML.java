46142ea2e00535f3d05af49a074a1f0395e4d637
roadmapqa <you@example.com>
2017-07-18 13:47:51 +0800

    Update script
    - Add regex expression to separate test step and expected result into
    - Fix executive pass parameter issue
    - Move the Document Object inside loop to fix the export all report
    issue
    - Change to outputSream instead of printWriter class to avoid log
    missing

--- src/main/java/synchronoss/com/FrontEndTool/TestLink/TransferExcelToXML.java
+++ src/main/java/synchronoss/com/FrontEndTool/TestLink/TransferExcelToXML.java
@@ -28,39 +28,49 @@ public class TransferExcelToXML {
   private final static String tableHeaderSummary = "Summary";
   private final static String tableHeaderSteps = "TestSteps";
   private final static String tableHeaderExpectedReseult = "ExpectedResult";
+  private final static String stepContectRegex =
+      "([\\d][\\. ][\\s+])|([\\.][\\s+])|([\\d][\\s+])|([\\d][\\.])|([\\d][\\)])|([\\)])|([\\.])";
 
   public static void main(String[] args) {
     String excelFile = args[0];
     String sheetNumberFromUser = args[1];
-    // String sheetNumberFromUser = "1";
-    // File caseFile = new File("C:\\Users\\roadmapqa\\Downloads\\WIT_Consolidated_testcases.xlsx");
+
     File caseFile = new File(excelFile);
     Workbook wb = null;
     int sheetNumber;
     int loopStartNumber = 2;
+    FileOutputStream errorLogis = null;
     try {
 
       // create file stream
       FileInputStream is = new FileInputStream(caseFile);
 
-      Document document = new Document();
-      document.setRootElement(new Element("testsuite"));
       wb = WorkbookFactory.create(is);
 
-      if (sheetNumberFromUser.equals("*")) {
+      if (sheetNumberFromUser.equals("all")) {
+        System.out.println("test");
         sheetNumber = wb.getNumberOfSheets();
+        System.out.println(sheetNumber);
       } else {
         sheetNumber = Integer.parseInt(sheetNumberFromUser);
         loopStartNumber = sheetNumber;
       }
-      
+
       // get the first sheet
       for (int i = loopStartNumber - 1; i < sheetNumber; i++) {
+
+        Document document = new Document();
+        document.setRootElement(new Element("testsuite"));
+
         File xmlFile2 = new File(wb.getSheetName(i) + ".xml");
+        File errorLog = new File("ErrorLog_" + wb.getSheetName(i) + ".txt");
+        errorLogis = new FileOutputStream(errorLog);
+
         List<String> columnHeaders = new ArrayList<String>();
         System.out.println(wb.getSheetName(i));
         Sheet sheetNum = wb.getSheetAt(i);
 
+        // iterator the first row
         for (Iterator<Row> rowsIT = sheetNum.rowIterator(); rowsIT.hasNext();) {
           Row row = rowsIT.next();
 
@@ -69,9 +79,10 @@ public class TransferExcelToXML {
           Element firstParentElement = new Element("testsuite");
           Element secondParentElement = new Element("testcase");
           Element testStepsElement = new Element("steps");
-          Element testStepElement = new Element("step");
 
-          // Iterate through the cells.
+          ArrayList<Element> stepList = new ArrayList<Element>();
+
+          // Iterate each cells.
           for (Iterator<Cell> cellsIT = row.cellIterator(); cellsIT.hasNext();) {
             emptyRow = false;
             Element childElement = null;
@@ -82,8 +93,7 @@ public class TransferExcelToXML {
               columnHeaders.add(cell.getStringCellValue());
 
             } else {
-
-              // remove the white space
+              // remove the white spaced of column header
               String elementTagName = columnHeaders.get(index).replaceAll("\\s+", "");
 
               // check with element tag
@@ -107,25 +117,66 @@ public class TransferExcelToXML {
                   continue;
                 }
                 case tableHeaderSteps: {
-                  Element stepNumberElement = new Element("step_number");
-                  stepNumberElement.addContent(new CDATA("1"));
-                  Element stepActionElement = new Element("actions");
+                  String stepContents = cell.getStringCellValue();
 
-                  stepActionElement.addContent(new CDATA(cell.getStringCellValue()));
-                  testStepElement.addContent(stepNumberElement);
-                  testStepElement.addContent(stepActionElement);
-                  testStepsElement.addContent(testStepElement);
+                  // split the test steps with return character and number
+                  String stepContent[] = stepContents.split("\n\\d");
+
+                  for (int j = 0; j < stepContent.length; j++) {
+                    Element testStepElement = new Element("step");
+                    Element stepNumberElement = new Element("step_number");
+                    stepNumberElement.addContent(new CDATA(String.valueOf(j + 1)));
+                    Element stepActionElement = new Element("actions");
+                    stepActionElement.addContent(
+                        new CDATA(stepContent[j].replaceFirst(stepContectRegex, "").trim()));
+
+                    testStepElement.addContent(stepNumberElement);
+                    testStepElement.addContent(stepActionElement);
+                    testStepsElement.addContent(testStepElement);
+                    stepList.add(testStepElement);
+                  }
                   secondParentElement.addContent(testStepsElement);
+
                   index++;
                   continue;
                 }
                 case tableHeaderExpectedReseult: {
-                  Element expectedResult = new Element("expectedresults");
-                  expectedResult.addContent(new CDATA(cell.getStringCellValue()));
-                  testStepElement.addContent(expectedResult);
+                  int loopStepSize = stepList.size();
+                  String expectedContents = cell.getStringCellValue();
+
+                  // split the expected result with return character and number
+                  String expectedContent[] = expectedContents.split("\n\\d");
+                  System.out.println(expectedContent.length);
+                  if (expectedContent.length != stepList.size()) {
+                    String errorInfo = "Sheet-" + wb.getSheetName(i)
+                        + ": The 'steps' and 'expected result' number in line "
+                        + (row.getRowNum() + 1) + " doesn't match \n";
+                    errorLogis.write(errorInfo.getBytes());
+                  }
+                  for (int j = 0; j < expectedContent.length; j++) {
+                    Element expectedResult = new Element("expectedresults");
+                    expectedResult.addContent(
+                        new CDATA(expectedContent[j].replaceFirst(stepContectRegex, "").trim()));
+                    if (j < loopStepSize) {
+                      stepList.get(j).addContent(expectedResult);
+                    } else {
+                      Element testStepElement = new Element("step");
+                      Element stepNumberElement = new Element("step_number");
+                      stepNumberElement.addContent(new CDATA(String.valueOf(j + 1)));
+                      Element stepActionElement = new Element("actions");
+
+                      testStepElement.addContent(stepNumberElement);
+                      testStepElement.addContent(stepActionElement);
+                      testStepsElement.addContent(testStepElement);
+                      stepList.add(testStepElement);
+                      testStepElement.addContent(expectedResult);
+                      stepList.add(testStepElement);
+                    }
+                  }
                   index++;
                   continue;
                 }
+
               }
 
               // if the cell content is empty, escape current loop
@@ -152,6 +203,7 @@ public class TransferExcelToXML {
         XMLOutputter xmlOutput = new XMLOutputter(format);
         xmlOutput.output(document, new FileOutputStream(xmlFile2));
       }
+      errorLogis.close();
     } catch (FileNotFoundException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
